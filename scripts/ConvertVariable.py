a6a39ad48bb2169c6c9ac8fc8b03ffddd8c92553
Marcin Zielak <mkzielak@gmail.com>
2023-05-28 21:43:30 +0200

    .

--- scripts/ConvertVariable.py
+++ scripts/ConvertVariable.py
@@ -42,52 +42,55 @@ with open(terraformtfvars, 'r') as f:
             account_number = re.sub('"', '',value_temp)
             break
 f.close()
-if re.search(r'preparing-variable', jobname):
-    with open("/root/project/env_account.var", "a") as f:
-    # write the key-value pair in the format "KEY=VALUE" to the end of the file
-        f.write(f"export {'ACCOUNT_NUMBER'}={account_number}\n")
-    f.close()
-    with open(terraformmainpath, 'r') as input_file:
-        # Open the output file in write mode
-        with open(terraformmainnewpath, 'w') as output_file:
-            # Loop over each line in the input file
-            for line in input_file:
-                # Check if this line contains the string you want to replace
-                if re.search(r'key.*tfstate"$', line):
-                    # Replace the line with the new string
-                    substring = '= "' + project_name + "/"
-                    new_line = re.sub(r'\s=\s"', substring, line)
-                    output_file.write(new_line)
-                else:
-                    # Write the original line to the output file
-                    output_file.write(line)
-        output_file.close()
-    input_file.close()                
-    os.rename(terraformmainpath,terraformmainoldpath)
-    os.rename(terraformmainnewpath,terraformmainpath)
-    os.remove(terraformmainoldpath)
+
+with open(terraformmainpath, 'r') as input_file:
+    # Open the output file in write mode
+    with open(terraformmainnewpath, 'w') as output_file:
+        # Loop over each line in the input file
+        for line in input_file:
+            # Check if this line contains the string you want to replace
+            if re.search(r'key.*tfstate"$', line):
+                # Replace the line with the new string
+                substring = '= "' + project_name + "/"
+                new_line = re.sub(r'\s=\s"', substring, line)
+                output_file.write(new_line)
+            else:
+                # Write the original line to the output file
+                output_file.write(line)
+    output_file.close()
+input_file.close()                
+os.rename(terraformmainpath,terraformmainoldpath)
+os.rename(terraformmainnewpath,terraformmainpath)
+os.remove(terraformmainoldpath)
 
 
 
-    session = boto3.Session()
-    client = session.client('sts')
-    try:
-        assumed_role = client.assume_role_with_web_identity(
-            RoleArn=rolearn_cfstackset,
-            RoleSessionName='AWS-KeyID',
-            WebIdentityToken=circle_oidc_token,
-        )
-    except ClientError as e:
-        print(e)
-        exit()
-
-    # Get temporary credentials
-    credentials = assumed_role['Credentials']
-
-    # Use the temporary credentials to access AWS resources
-    iam_client = boto3.client(
-        'iam',
-        aws_access_key_id=credentials['AccessKeyId'],
-        aws_secret_access_key=credentials['SecretAccessKey'],
-        aws_session_token=credentials['SessionToken']
+session = boto3.Session()
+client = session.client('sts')
+try:
+    assumed_role = client.assume_role_with_web_identity(
+        RoleArn=rolearn_cfstackset,
+        RoleSessionName='AWS-KeyID',
+        WebIdentityToken=circle_oidc_token,
     )
+except ClientError as e:
+    print(e)
+    exit()
+
+# Get temporary credentials
+credentials = assumed_role['Credentials']
+
+# Use the temporary credentials to access AWS resources
+# iam_client = boto3.client(
+#     'iam',
+#     aws_access_key_id=credentials['AccessKeyId'],
+#     aws_secret_access_key=credentials['SecretAccessKey'],
+#     aws_session_token=credentials['SessionToken']
+#     )
+  
+with open("/root/project/env_account.var", "a") as f:
+   # write the key-value pair in the format "KEY=VALUE" to the end of the file
+    f.write(f"export {'AWS_ACCESS_KEY_ID'}={credentials['AccessKeyId']}\n")
+    f.write(f"export {'AWS_SECRET_ACCESS_KEY'}={credentials['SecretAccessKey']}\n")
+    f.write(f"export {'AWS_SESSION_TOKEN'}={credentials['SessionToken']}\n")
+f.close()
